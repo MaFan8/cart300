34c445b2928b1b5ff84ac8919806ffacd3d15355
MaFan8 <maple_f@hotmail.com>
2021-09-26 21:48:31 -0400

    Updated click event

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
+window.onload = function() {
 
-window.onload = function(){
+  document.getElementById('exercises').addEventListener("click", mouseClickHandler);
 
-document.getElementById('exercises').addEventListener("click", mouseClickHandler);
+  function mouseClickHandler() {
+    let presentation = document.getElementById('presentation');
+    let reflections = document.getElementById('reflections');
+    let project = document.getElementById('project');
 
-function mouseClickHandler() {
-  document.getElementById('presentation').hidden = true;
-  document.getElementById('reflections').hidden = true;
-  document.getElementById('project').hidden = true;
-}
-
+    if (presentation.style.display === "none") {
+      presentation.style.display = "block";
+      reflections.style.display = "block";
+      project.style.display = "block";
+    } else {
+      presentation.style.display = "none";
+      reflections.style.display = "none";
+      project.style.display = "none";
+    }
+  }
 }
