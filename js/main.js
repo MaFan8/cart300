78bc2900f0fad5178c09531680c50866ebc62088
MaFan8 <maple_f@hotmail.com>
2021-10-06 20:50:25 -0400

    Made subtitles toggle with counter

--- js/main.js
+++ js/main.js
@@ -4,53 +4,72 @@ let exercises = document.getElementById('exercises');
 let presentation = document.getElementById('presentation');
 let reflections = document.getElementById('reflections');
 let project = document.getElementById('project');
-
+let clickCount =0;
 
 document.getElementById('exercises').addEventListener('click', function() {
+  if(clickCount%2==0){
   presentation.style.visibility = "hidden";
   reflections.style.visibility = "hidden";
   project.style.visibility = "hidden";
-  //remove hover in css
-  exercises.style.pointerEvents = "none";
   //unhide lists
   document.getElementById('ex_links').style.display = "block";
+}
+else{
+  presentation.style.visibility = "visible";
+  reflections.style.visibility = "visible";
+  project.style.visibility = "visible";
+  //unhide lists
+  document.getElementById('ex_links').style.display = "none";
+}
+  clickCount = clickCount+1;
   //show dots
   // document.getElementById('dots').style.display = "block";
 });
 
 document.getElementById('presentation').addEventListener('click', function() {
+  if (clickCount%2==0) {
   exercises.style.visibility = "hidden";
   reflections.style.visibility = "hidden";
   project.style.visibility = "hidden";
-  //remove hover in css
-  presentation.style.pointerEvents = "none";
   //unhide lists
-  document.getElementById('slides_link').style.display = "block";
+  // document.getElementById('slides_link').style.display = "block";
   document.getElementById('presentation_links').style.display = "block";
-});
-
-document.getElementById('reflections').addEventListener('click', function() {
-  presentation.style.visibility = "hidden";
-  exercises.style.visibility = "hidden";
-  project.style.visibility = "hidden";
-  //remove hover in css
-  reflections.style.pointerEvents = "none";
+}
+else {
+  exercises.style.visibility = "visible";
+  reflections.style.visibility = "visible";
+  project.style.visibility = "visible";
   //unhide lists
-  document.getElementById('ref_links1').style.display = "block";
-  document.getElementById('ref_links2').style.display = "block";
+  // document.getElementById('slides_link').style.display = "block";
+  document.getElementById('presentation_links').style.display = "none";
+}
+  clickCount = clickCount+1;
+  //show dots
+  // document.getElementById('dots').style.display = "block";
 });
-
-document.getElementById('project').addEventListener('click', function() {
-  exercises.style.visibility = "hidden";
-  reflections.style.visibility = "hidden";
-  presentation.style.visibility = "hidden";
-  //remove hover in css
-  project.style.pointerEvents = "none";
-  //unhide lists
-  document.getElementById('proposal_link').style.display = "block";
-  document.getElementById('middle_links').style.display = "block";
-  document.getElementById('final_link').style.display = "block";
-});
+//
+// document.getElementById('reflections').addEventListener('click', function() {
+//   presentation.style.visibility = "hidden";
+//   exercises.style.visibility = "hidden";
+//   project.style.visibility = "hidden";
+//   //remove hover in css
+//   reflections.style.pointerEvents = "none";
+//   //unhide lists
+//   document.getElementById('ref_links1').style.display = "block";
+//   document.getElementById('ref_links2').style.display = "block";
+// });
+//
+// document.getElementById('project').addEventListener('click', function() {
+//   exercises.style.visibility = "hidden";
+//   reflections.style.visibility = "hidden";
+//   presentation.style.visibility = "hidden";
+//   //remove hover in css
+//   project.style.pointerEvents = "none";
+//   //unhide lists
+//   document.getElementById('proposal_link').style.display = "block";
+//   document.getElementById('middle_links').style.display = "block";
+//   document.getElementById('final_link').style.display = "block";
+// });
 
 
 }
